b780d458301b9ffbaf105c04e17797c15a0750b1
nsant002 <156372756+nsant002@users.noreply.github.com>
2024-12-01 17:10:46 -0500

    recent commit 12-1-24

--- client/src/component/login.jsx
+++ client/src/component/login.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { Link, useNavigate } from 'react-router-dom';
+import ReCAPTCHA from "react-google-recaptcha";
 
 const Login = () => {
     const [email, setEmail] = useState('');
