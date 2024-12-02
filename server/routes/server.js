3d62e996ef332122818ffdf1136666be8955e5b4
nsant002 <156372756+nsant002@users.noreply.github.com>
2024-12-02 18:40:08 -0500

    reinstalled jwt

--- server/routes/server.js
+++ server/routes/server.js
@@ -26,7 +26,7 @@
 
 import express from "express";
 import bcrypt from "bcrypt";
-//import jwt from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 import cors from "cors";
 import crypto from "crypto";
 import { Router } from "express";
