e87344faf2838701f52efb1c0068d8e6662fd9a3
nsant002 <156372756+nsant002@users.noreply.github.com>
2024-12-02 23:13:56 -0500

    change of clickjacking code in main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,17 +3,9 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-// Check if the app is being loaded inside an iframe
 if (window.self !== window.top) {
-  document.body.innerHTML = `
-  <div style="display: flex; justify-content: center; align-items: center; height:
-  100vh; background-color: white;">
-  <h1 style="color: red; font-family: Arial, sans-serif;">This content cannot be
-  displayed in an iframe.</h1>
-  </div>
-  `;
-  throw new Error("This content cannot be displayed in an iframe.");
-  }
+  throw new Error("iFrame detected!");
+}
   
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
