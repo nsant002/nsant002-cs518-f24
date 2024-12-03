4671786784aba69ebc5fae14e2590c7734fe9119
nsant002 <156372756+nsant002@users.noreply.github.com>
2024-12-03 15:39:33 -0500

    adding additional test case

--- server/test/test.js
+++ server/test/test.js
@@ -2,22 +2,6 @@ import { expect } from "chai";
 import supertest from "supertest";
 import app from "../app.js";
 
-describe('Addition Testing', ()=>{
-
-    it('Should Add two numbers',function(){
-        var num1=2;
-        var num2=5;
-        expect(num1+num2).equal(7)
-    })
-
-    it('Should Add 3 numbers',function(){
-        var num1=2;
-        var num2=5;
-        var num3=1;
-        expect(num1+num2+num3).equal(8)
-    })
-
-})
 
 
 describe('Testing Successful Login', ()=>{
@@ -43,5 +27,16 @@ describe('Testing Login Failure', ()=>{
         })
 
         expect(response.status).equal(401)
+    })
+})   
+
+describe('Testing Prerequisites API', ()=>{
+
+    it('Testing Prerequisites API',async function(){
+       
+        var response= await supertest(app).get(`/server/prerequisites`).send({         
+        })
+
+        expect(response.status).equal(200)
     })
 })   
